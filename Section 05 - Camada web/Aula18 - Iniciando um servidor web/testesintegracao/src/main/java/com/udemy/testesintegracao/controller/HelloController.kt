04a5794d3204e59b55b09e876cab519e6d543f31
unknown <jair.lopes@fatec.sp.gov.br>
2022-07-09 20:07:02 -0300

    Iniciando um servidor web para os testes

---
+++ Section 05 - Camada web/Aula18 - Iniciando um servidor web/testesintegracao/src/main/java/com/udemy/testesintegracao/controller/HelloController.kt
@@ -0,0 +1,33 @@
+package com.udemy.testesintegracao.controller
+
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+public class HelloController {
+
+    @ResponseStatus(value = HttpStatus.OK)
+    @GetMapping("/hello")
+    public String getHello(){
+        return "Hello";
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Section 05 - Camada web/Aula18 - Iniciando um servidor web/testesintegracao/src/test/java/com/udemy/testesintegracao/TestesintegracaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.testesintegracao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestesintegracaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
